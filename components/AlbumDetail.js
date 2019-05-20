e8e05ebba8b5ed5a29417220ad94b95b7e142843
Vidhya Paliwal <vidhya.paliwal@gmail.com>
2019-05-20 17:37:07 +0530

    Initial commit
    Includes features to display list of songs.
    - Play/Pause button to play and pause a song.
    - Added button having link to the youtube location.

---
+++ components/AlbumDetail.js
@@ -0,0 +1,94 @@
+import React from 'react';
+import { Text, View, Image, Linking } from 'react-native';
+import Card from './Card';
+import CardSection from './CardSection';
+import Button from './Button';
+import { ReactNativeAudioStreaming } from 'react-native-audio-stream';
+
+var song = null;
+
+var BASE_URL = "http://storage.googleapis.com/automotive-media/";
+
+class AlbumDetail extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      playMessage : "Play"
+    };
+  }
+
+  playPauseSong() {
+    if (this.state.playMessage == "Play") {
+      ReactNativeAudioStreaming.play(BASE_URL + this.props.song.source, {});
+      this.setState({playMessage: "Pause"});
+    }
+    else {
+      ReactNativeAudioStreaming.pause();
+      this.setState({playMessage: "Play"});
+    }
+  }
+
+  render() {
+    return (
+      <Card>
+        <CardSection>
+          <View style={styles.thumbnailContainerStlye}>
+          <Image
+            style={styles.thumbnailStyle}
+            source={{ uri: BASE_URL + this.props.song.image }}
+          />
+          </View>
+          <View>
+             <Text style={styles.headerTextStyle}>{this.props.song.title}</Text>
+             <Text>{this.props.song.artist}</Text>
+          </View>
+        </CardSection>
+
+        <CardSection>
+           <Image
+             style={styles.imageStyle}
+             source={{ uri: BASE_URL + this.props.song.image }}
+           />
+        </CardSection>
+
+        <CardSection>
+            <Button onPress={() => this.playPauseSong()}>
+               {this.state.playMessage}
+            </Button>
+            <Button onPress={() => Linking.openURL(this.props.song.site)}>
+               View Online
+            </Button>
+        </CardSection>
+      </Card>
+    );
+  }
+}
+
+const styles = {
+  headerContentStyle: {
+    flexDirection: 'column',
+    justifyContent: 'space-around'
+  },
+  headerTextStyle: {
+    fontSize: 18
+  },
+  thumbnailStyle: {
+    height: 50,
+    width: 50
+  },
+  thumbnailContainerStlye: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginLeft: 10,
+    marginRight: 10
+  },
+  imageStyle: {
+    height: 300,
+    width: null,
+    flex: 1
+  }
+};
+
+
+
+export default AlbumDetail;
